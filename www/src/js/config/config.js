f245bbe21ae1125ee024a1327eb9d1089da47a7b
chunzhi-yang <872373354@qq.com>
2017-04-18 00:03:20 +0800

    提交界面

---
+++ www/src/js/config/config.js
@@ -0,0 +1,5 @@
+app.constant('Config',{
+	serverUrl:'http://192.168.1.7:8080/book-manager-api/',	
+    avatarPrefix:'http:// 192.168.1.7:8080/app/downloadAvatar?url=',
+    photoPrefix:'http:// 192.168.1.7:8080/app/downloadPhoto?url='
+});
---
+++ www/src/js/controllers/self/selfCtrl.js
@@ -0,0 +1,4 @@
+app.controller('selfCtrl', function($scope, $http) {
+
+   
+});
